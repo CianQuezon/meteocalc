45c679de1ac0896be70756afb94ca251794b2a96
Cian Quezon <cdsquezon@gmail.com>
2026-02-16 23:17:06 +0000

    Added the registry for approximation functions

---
+++ src/meteocalc/dewpoint/_types.py
@@ -0,0 +1,13 @@
+"""
+Docstring for meteocalc.dewpoint._types
+"""
+from meteocalc.dewpoint._enums import DewPointEquationName
+from meteocalc.dewpoint._dewpoint_equations import MagnusDewpointEquation, VaporInversionDewpoint
+
+APPROXIMATION_DEWPOINT_REGISTRY = {
+    DewPointEquationName.MAGNUS: MagnusDewpointEquation
+}
+
+SOLVER_DEWPOINT_REGISTRY = {
+    DewPointEquationName.VAPOR_INVERSION: VaporInversionDewpoint
+}
