768ef60eaeec2e30f1571c8c40421cc75b26f1b7
Cian Quezon <cdsquezon@gmail.com>
2026-02-21 00:41:09 +0000

    Ruff formatted the files

--- src/meteocalc/vapor/_types.py
+++ src/meteocalc/vapor/_types.py
@@ -5,11 +5,14 @@ Author: Cian Quezon
 """
 
 from meteocalc.vapor._enums import VaporEquationName
-from meteocalc.vapor._vapor_equations import BoltonEquation, GoffGratchEquation, HylandWexlerEquation
+from meteocalc.vapor._vapor_equations import (
+    BoltonEquation,
+    GoffGratchEquation,
+    HylandWexlerEquation,
+)
 
 EQUATION_REGISTRY = {
     VaporEquationName.BOLTON: BoltonEquation,
     VaporEquationName.GOFF_GRATCH: GoffGratchEquation,
     VaporEquationName.HYLAND_WEXLER: HylandWexlerEquation,
 }
-
