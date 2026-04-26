e9a9599f48dbbd2a77178a5b49bda9d83cbd7a3d
Cian Quezon <cdsquezon@gmail.com>
2026-04-26 18:06:23 +0100

    src/meteocalc/lcl/_solver_method.py

---
+++ src/meteocalc/shared/constants.py
@@ -0,0 +1,11 @@
+"""
+Docstring for meteocalc.shared.constants
+"""
+
+
+Rd = 287.05  # Gas constant for dry air
+Rv = 461.5  # Gas constant for water vapour
+cpd = 1004.5 # Specific heat of dry air at constant pressure
+
+eps = Rd/Rv # Ratio of gas constants
+
