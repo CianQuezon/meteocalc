0d31c49b54ff51146e9e5c645faa1cb7410527a5
Cian Quezon <cdsquezon@gmail.com>
2026-04-26 18:12:53 +0100

    Ruff formatted constants.py

--- src/meteocalc/shared/constants.py
+++ src/meteocalc/shared/constants.py
@@ -2,10 +2,8 @@
 Docstring for meteocalc.shared.constants
 """
 
-
 Rd = 287.05  # Gas constant for dry air
 Rv = 461.5  # Gas constant for water vapour
-cpd = 1004.5 # Specific heat of dry air at constant pressure
+cpd = 1004.5  # Specific heat of dry air at constant pressure
 
-eps = Rd/Rv # Ratio of gas constants
-
+eps = Rd / Rv  # Ratio of gas constants
